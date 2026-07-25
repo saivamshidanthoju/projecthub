443e43181c1cd1716857c6a2a57466ef232dc636
saivamshidanthoju <saivamshidanthoju@gmail.com>
2026-07-25 14:30:55 +0530

    ProjectHub: Restructure project into backend and frontend

---
+++ backend/src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "ProjectHub Backend Running 🚀"
+    });
+});
+
+module.exports = app;
